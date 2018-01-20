daaa490eae4240d6e251aa9781f22fd353317697
Aliaksei Boole <boole@gojuno.com>
2018-01-20 17:47:28 +0300

    Api for clean whole database were added.

--- src/main.py
+++ src/main.py
@@ -7,5 +7,6 @@ app = web.Application(middlewares=[middlewares.process_error])
 app.router.add_get('/r/{link_id}', handlers.redirect)
 app.router.add_post('/shortcut', handlers.shortcut)
 app.router.add_post('/stats', handlers.get_stats)
+app.router.add_delete('/admin/purge', handlers.purge_all)
 
 web.run_app(app, port=USH_PORT)
